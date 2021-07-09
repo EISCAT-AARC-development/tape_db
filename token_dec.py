123fca90abd5085b8c223f301b7cd0199368d47d
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2021-07-09 11:31:26 +0000

    Debugged

--- token_dec.py
+++ token_dec.py
@@ -3,15 +3,16 @@ from jwt import decode
 from os import environ
 from cryptography.hazmat.primitives import serialization
 
+
 ### Decode routine
 def token_dec(token):                                                                                                                                                  
     # Read public key
     try:
-        with open(os.environ["TOKEN_SIGNING_PUB_KEY_PATH"], 'rb') as public_key:
-            pub_key = serialization.load_ssh_public_key(public_key.read())
+        with open(environ["TOKEN_SIGNING_PUB_KEY_PATH"], 'rb') as pkey:
+            public_key = serialization.load_ssh_public_key(pkey.read())
     except:
+        print("Reading key failed")
         raise IOError("Could not read public key for validation")
-    
     # Validate and decode
-    out = decode(token, key=pub_key, algorithms=['RS256', ])
+    out = decode(token, key=public_key, algorithms=['RS256', ])
     return(out)
